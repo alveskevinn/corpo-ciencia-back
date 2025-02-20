09de6be1bbc4649a39cfac1dacd1e266ea0fbb11
Alveskevinn <kevinpedrosodev@gmail.com>
2025-02-20 08:38:16 -0300

    fix: service athlete

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
+// import express from 'express'
+// import cors from 'cors'
+// import dotenv from 'dotenv'
+// import athleteRoutes from './routes/athlete.routes'
+
+// dotenv.config()
+
+// const app = express()
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.use('/athletes', athleteRoutes)
+
+// app.get('/', (req, res) => {
+//   res.send('API funcionando!')
+// })
+
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`)
+// })
+
+
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import athleteRoutes from './routes/athlete.routes'
+import { Client } from 'pg'
+import athleteRoutes from './routes/athlete.routes'  
 
 dotenv.config()
 
@@ -10,11 +35,23 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/athletes', athleteRoutes)
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+})
+
+client.connect()
+  .then(() => console.log('Conectado ao banco de dados PostgreSQL'))
+  .catch((err) => console.error('Erro de conexão com o banco de dados:', err.stack))
 
 app.get('/', (req, res) => {
   res.send('API funcionando!')
 })
+
+app.use('/athletes', athleteRoutes)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
--- src/services/athlete.service.ts
+++ src/services/athlete.service.ts
@@ -4,7 +4,7 @@ import { Athlete } from '../models/athlete.model'
 export const AthleteService = {
   create: async ({ firstName, email, status }: Omit<Athlete, 'id'>) => {
     const result = await pool.query(
-      'INSERT INTO athletes (first_name, email, status) VALUES ($1, $2, $3) RETURNING *',
+      'INSERT INTO athletes (firstname, email, status) VALUES ($1, $2, $3) RETURNING *',
       [firstName, email, status]
     )
     return result.rows[0]
