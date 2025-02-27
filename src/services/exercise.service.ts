1f6caae087b749b75ef10dc2dd531877aed395c3
PedroH <pedroh422silva@gmail.com>
2025-02-27 11:57:59 -0300

    mudanças

--- src/services/exercise.service.ts
+++ src/services/exercise.service.ts
@@ -14,24 +14,24 @@ class ExerciseService {
   }
 
   async createExercise(exercise: Exercise): Promise<Exercise> {
-    const { name, description, category, group_muscular, video_url } = exercise;
+    const { name, musculo_principal, category, group_muscular, video_url, auxiliar_1, auxiliar_2 } = exercise;
     const result = await pool.query(
-      'INSERT INTO exercicios (nome, descricao, categoria, grupo_muscular, video_url) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [name, description, category, group_muscular, video_url]
+      `INSERT INTO exercicios (nome, musculo_principal, categoria, grupo_muscular, video_url, auxiliar_1, auxiliar_2) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+      [name, musculo_principal, category, group_muscular, video_url, auxiliar_1, auxiliar_2]
     );
     return result.rows[0];
   }
-  
 
   async updateExercise(id: number, exercise: Exercise): Promise<Exercise | null> {
-    const { name, description, category, group_muscular, video_url } = exercise;
+    const { name, musculo_principal, category, group_muscular, video_url, auxiliar_1, auxiliar_2 } = exercise;
     const result = await pool.query(
-      'UPDATE exercicios SET nome = $1, descricao = $2, categoria = $3, grupo_muscular = $4, video_url = $5 WHERE id = $6 RETURNING *',
-      [name, description, category, group_muscular, video_url, id]
+      `UPDATE exercicios SET nome = $1, musculo_principal = $2, categoria = $3, grupo_muscular = $4, video_url = $5, 
+      auxiliar_1 = $6, auxiliar_2 = $7 WHERE id = $8 RETURNING *`,
+      [name, musculo_principal, category, group_muscular, video_url, auxiliar_1, auxiliar_2, id]
     );
     return result.rows[0] || null;
-  }
-  
+  }  
 
   async deleteExercise(id: number): Promise<boolean> {
     const result = await pool.query('DELETE FROM exercicios WHERE id = $1', [id]);
