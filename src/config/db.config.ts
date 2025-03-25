dc550f6462fb6627298fcec83e7ebeecd25ec8ed
Alveskevinn <kevinpedrosodev@gmail.com>
2025-03-25 17:41:55 -0300

    ajustes

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -6,7 +6,7 @@ dotenv.config()
 console.log('🔍 Variáveis de ambiente carregadas:')
 console.log('DB_USER:', process.env.DB_USERNAME)
 console.log('DB_HOST:', process.env.DB_HOST)
-console.log('DB_NAME:', process.env.DB_USERNAME)
+console.log('DB_NAME:', process.env.DB_DATABASE)
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '***' : 'NÃO DEFINIDO')
 console.log('DB_PORT:', process.env.DB_PORT)
 
