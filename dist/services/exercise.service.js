d07b994c181ac3a17949e42914e062d3b28ab3f0
Alveskevinn <kevinpedrosodev@gmail.com>
2025-03-26 15:50:49 -0300

    fix: ajustes

--- dist/services/exercise.service.js
+++ dist/services/exercise.service.js
@@ -6,53 +6,51 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const db_config_1 = __importDefault(require("../config/db.config"));
 class ExerciseService {
     async getAllExercises() {
-        const [rows] = await db_config_1.default.execute('SELECT * FROM exercicios');
+        const [rows] = await db_config_1.default.execute('SELECT * FROM exercises');
         return rows;
     }
     async getExerciseById(id) {
-        const [rows] = await db_config_1.default.execute('SELECT * FROM exercicios WHERE id = ?', [
+        const [rows] = await db_config_1.default.execute('SELECT * FROM exercises WHERE id = ?', [
             id,
         ]);
         return rows.length > 0 ? rows[0] : null;
     }
     async createExercise(exercise) {
-        const { exercicio_principal, video_url, exercicio_aux1, exercicio_aux2, } = exercise;
+        const { exercicio, musculo_principal, video, musculo_aux1, musculo_aux2, } = exercise;
         const sanitizedValues = [
-            exercicio_principal ?? null,
-            video_url ?? null,
-            exercicio_aux1 ?? null,
-            exercicio_aux2 ?? null,
+            exercicio ?? null,
+            musculo_principal ?? null,
+            video ?? null,
+            musculo_aux1 ?? null,
+            musculo_aux2 ?? null,
         ];
-        const query = `INSERT INTO exercicios (exercicio_principal, video_url, exercicio_aux1, exercicio_aux2) 
-                   VALUES (?, ?, ?, ?)`;
+        const query = `INSERT INTO exercises (exercicio, musculo_principal, video, musculo_aux1, musculo_aux2) 
+                   VALUES (?, ?, ?, ?, ?)`;
         try {
             const [result] = await db_config_1.default.execute(query, sanitizedValues);
             const insertedId = result.insertId;
-            const [rows] = await db_config_1.default.execute('SELECT * FROM exercicios WHERE id = ?', [insertedId]);
+            const [rows] = await db_config_1.default.execute('SELECT * FROM exercises WHERE id = ?', [insertedId]);
             return rows[0];
         }
         catch (error) {
-            // Logando o erro completo
-            console.error('Error creating exercise:', error);
-            // Jogando o erro novamente com detalhes
             throw new Error(`Error creating exercise: ${JSON.stringify(error)}`);
         }
     }
     async updateExercise(id, exercise) {
-        const { name, musculo_principal, group_muscular, video_url, exercicio_aux1, exercicio_aux2 } = exercise;
-        const query = `UPDATE exercicios SET nome = ?, musculo_principal = ?,  grupo_muscular = ?, video_url = ?, 
-                   exercicio_aux1 = ?, exercicio_aux2 = ? WHERE id = ?`;
-        const [result] = await db_config_1.default.execute(query, [name, musculo_principal, group_muscular, video_url, exercicio_aux1, exercicio_aux2, id]);
+        const { exercicio, musculo_principal, video, musculo_aux1, musculo_aux2 } = exercise;
+        const query = `UPDATE exercises SET exercicio = ?, musculo_principal = ?, video = ?, musculo_aux1 = ?, 
+                   musculo_aux2 = ? WHERE id = ?`;
+        const [result] = await db_config_1.default.execute(query, [exercicio, musculo_principal, video, musculo_aux1, musculo_aux2, id]);
         if (result.affectedRows === 0)
             return null;
-        const [rows] = await db_config_1.default.execute('SELECT * FROM exercicios WHERE id = ?', [id]);
+        const [rows] = await db_config_1.default.execute('SELECT * FROM exercises WHERE id = ?', [id]);
         return rows[0] || null;
     }
     async deleteExercise(id) {
-        const [result] = await db_config_1.default.execute('DELETE FROM exercicios WHERE id = ?', [
+        const [result] = await db_config_1.default.execute('DELETE FROM exercises WHERE id = ?', [
             id,
         ]);
-        const affectedRows = result.affectedRows; // A propriedade 'affectedRows' está dentro de 'result'
+        const affectedRows = result.affectedRows;
         return affectedRows > 0;
     }
 }
